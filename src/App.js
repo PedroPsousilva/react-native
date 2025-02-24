0fcc94078d60ab7939fe5a67b348b9d0d83cf4be
PedroPsousilva <pedro.soupsilva@gmail.com>
2025-02-24 14:54:27 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+//import Login from './screens/Login';
+import Cadastro from './screens/Cadastro';
+
+//export default function App() {
+  //return (
+    //<View style={styles.container}>
+     //<Login></Login>
+    //</View>
+  //);
+//}
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Cadastro/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  box1:{
+    width: 50,
+    height: 50,
+    backgroundColor: "pink"
+  }, box2:{
+    width: 50,
+    height: 50,
+    backgroundColor: "blue"
+  }, box3:{
+    width: 50,
+    height: 50,
+    backgroundColor: "purple"
+  }, box4:{
+    width: 50,
+    height: 50,
+    backgroundColor: "red"
+  }, row:{
+    flexDirection:"row"
+  },text:{
+    fontSize:28,
+    fontWeight: 'bold'
+  }, input:{
+    borderWidth:1,
+    borderColor:'gray',
+    width:'80%',
+    padding:10,
+    marginVertical:10,
+  }
+
+
+});
